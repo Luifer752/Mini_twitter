085495ed2d751fb388ab6bcbfd532d3bf5a06a5b
Luifer752 <turutututututu@gmail.com>
2023-10-15 13:14:59 -0500

    Users app added

--- mini_twitter/mini_twitter/settings.py
+++ mini_twitter/mini_twitter/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mini_twitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'twitter_db',
+        'USER': 'mini_twitter',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
