5d7834b6c5558cd242667511255a09ca1d9a1165
Luifer752 <turutututututu@gmail.com>
2023-10-15 14:07:59 -0500

    Comment model added

--- mini_twitter/posts/models.py
+++ mini_twitter/posts/models.py
@@ -8,3 +8,11 @@ class Posts(models.Model):
     content = models.CharField(max_length=128)
 
     crated_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    content = models.CharField(max_length=128)
+
+    crated_at = models.DateTimeField(auto_now_add=True)
