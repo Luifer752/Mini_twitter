a7d1013a6f30ac3111a1fdd42302e85c1f23c9fb
Luifer752 <turutututututu@gmail.com>
2023-10-15 13:19:39 -0500

    Users Model added

--- mini_twitter/users/models.py
+++ mini_twitter/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    date_joined = models.DateTimeField(auto_now_add=True)
