7ec62fcf48c4f23ac43500750fff61dc3cf1f548
Luifer752 <turutututututu@gmail.com>
2024-01-04 14:37:30 -0600

    Need to finish CreateView

--- mini_twitter/posts/urls.py
+++ mini_twitter/posts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView, PostCreateView, comments_log, post_comments, add_comment
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name="posts_list"),
-    # path('add-post', add_post, name='add_post'),
-    # path('<str:user_or_id>', filtered_posts, name="filtered_posts"),
-    # path('comments/', comments_log, name="comments_log"),
-    # path('comments/<str:post_id>', post_comments, name="post_comments"),
-    # path('add_comment/', add_comment, name='add_comment')
+    path('add-post', PostCreateView.as_view(), name='add_post'),
+    path('<int:pk>', PostDetailView.as_view(), name="posts_details"),
+    path('comments/', comments_log, name="comments_log"),
+    path('comments/<str:post_id>', post_comments, name="post_comments"),
+    path('add_comment/', add_comment, name='add_comment')
 ]
--- mini_twitter/posts/views.py
+++ mini_twitter/posts/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Posts, Comment
 from .forms import CommentForm, PostForm
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 
 class PostListView(ListView):
@@ -10,16 +11,30 @@ class PostListView(ListView):
     template_name = 'posts/posts_adn_comments.html'
     context_object_name = 'posts'
 
-    def get_queryset(self):
-        post_id = self.kwargs['post_id']
-        return Posts.objects.filter(post_id=post_id)
+    # def get_queryset(self):
+    #     post_id = self.kwargs['post_id']
+    #     return Posts.objects.filter(post_id=post_id)
+    #
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['posts'] = Posts.objects.all()
+    #     context['posts'] = Posts.objects.get(pk=self.kwargs['post_id'])
+    #     return context
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['posts'] = Posts.objects.all()
-        context['posts'] = Posts.objects.get(pk=self.kwargs['post_id'])
-        return context
 
+class PostDetailView(DetailView):
+    model = Posts
+    template_name = 'posts/posts_adn_comments.html'
+    context_object_name = 'post'
+
+
+class PostCreateView(CreateView):
+    model = Posts
+    form_class = PostForm
+    template_name = 'posts/add_post.html'
+
+    def get_success_url(self):
+        return reverse_lazy('books_list')
 class CommentListView(ListView):
     model = Comment
     template_name = 'posts/posts_adn_comments.html'
@@ -32,54 +47,39 @@ class CommentListView(ListView):
 #     return render(request, 'posts/posts_adn_comments.html', context)
 #
 #
-# def comments_log(request):
-#     comments = Comment.objects.all()
-#     context = {'comments': comments}
-#     return render(request, 'posts/comments_list.html', context)
-#
-#
-# def post_comments(request, post_id=None):
-#
-#     if post_id:
-#         comments = Comment.objects.filter(post__pk=post_id)
-#
-#     context = {'comments': comments}
-#     return render(request, 'posts/comments_list.html', context)
-#
-#
-# def filtered_posts(request, user_or_id=None):
-#
-#     if user_or_id.isdigit():
-#         posts = Posts.objects.filter(pk=user_or_id)
-#     elif not user_or_id.isdigit():
-#         user_or_id = user_or_id.capitalize()
-#         posts = Posts.objects.filter(user__username=user_or_id)
-#
-#     context = {'posts': posts, 'title': 'Available posts'}
-#     return render(request, 'posts/posts_adn_comments.html', context)
-#
-#
-# def add_post(request):
-#
-#     if request.method == 'POST':
-#         form = PostForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             print(form.cleaned_data)
-#             post = Posts.objects.create(**form.cleaned_data)
-#             return redirect('filtered_posts', user_or_id=post.pk)
-#     else:
-#         form = PostForm()
-#     return render(request, 'posts/add_post.html', {'form': form})
-#
-#
-# def add_comment(request):
-#
-#     if request.method == 'POST':
-#         comment_form = CommentForm(request.POST)
-#         if comment_form.is_valid():
-#
-#             comment = comment_form.save()
-#             return redirect('post_comments', post_id=comment.post.pk)
-#     else:
-#         comment_form = CommentForm()
-#     return render(request, 'posts/add_comment.html', {'comment_form': comment_form})
+def comments_log(request):
+    comments = Comment.objects.all()
+    context = {'comments': comments}
+    return render(request, 'posts/comments_list.html', context)
+
+
+def post_comments(request, post_id=None):
+
+    if post_id:
+        comments = Comment.objects.filter(post__pk=post_id)
+
+    context = {'comments': comments}
+    return render(request, 'posts/comments_list.html', context)
+
+
+def filtered_posts(request, user_or_id=None):
+
+    if user_or_id.isdigit():
+        posts = Posts.objects.filter(pk=user_or_id)
+    elif not user_or_id.isdigit():
+        user_or_id = user_or_id.capitalize()
+        posts = Posts.objects.filter(user__username=user_or_id)
+
+    context = {'posts': posts, 'title': 'Available posts'}
+    return render(request, 'posts/posts_adn_comments.html', context)
+
+def add_comment(request):
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+
+            comment = comment_form.save()
+            return redirect('post_comments', post_id=comment.post.pk)
+    else:
+        comment_form = CommentForm()
+    return render(request, 'posts/add_comment.html', {'comment_form': comment_form})
